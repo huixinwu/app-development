96ba09e9b137e682d91c2a0e12e1723b1bdd8845
professorWu <hwu@qcc.cuny.edu>
2024-07-18 20:46:20 -0400

    lab 6 - react routing

---
+++ lab6_testing/src/components/contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = function(){
+    return(
+        <>
+            <header className='header'>
+                <h1 className='headertitle'>Contact us</h1>
+            </header>
+        </>
+    )
+}
+
+export default Contact
