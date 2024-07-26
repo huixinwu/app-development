6a109375f031322c52e92d8ed469b2c6c77d0c61
professorWu <hwu@qcc.cuny.edu>
2024-07-25 20:50:14 -0400

    lab 9 - js object and nodejs server

---
+++ lab9_objects_nodejsrouting/app.js
@@ -0,0 +1,43 @@
+// import the http module
+const http = require('http')
+const fs = require('fs')
+
+// create server
+const server = http.createServer(function(request, response){
+    response.writeHead(200,{'Content-Type':'text/html'})
+
+    //create routing using url
+    const url = request.url
+
+    if(url ==='/home' || url === '/'){
+        fs.createReadStream('index.html').pipe(response)
+    }
+    else if (url==='/about'){
+        fs.createReadStream('about.html').pipe(response)
+    }
+    else{
+        fs.createReadStream('404.html').pipe(response)
+    }
+
+})
+
+// server is listening to port 3000
+server.listen(3100,function(){
+    console.log('Server is running')
+})
+
+
+
+
+
+/*
+// server for index.html
+const server = http.createServer(function(request, response){
+    response.writeHead(200,{'Content-Type':'text/html'})
+    const readStream = fs.createReadStream(__dirname + '/index.html')
+
+    // piping
+    readStream.pipe(response)
+})
+
+*/
